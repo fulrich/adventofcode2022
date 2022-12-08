f2f7d1fb2a9412426cd419df61a85aaf1aa3a17a
Fred Ulrich <9284621+fulrich@users.noreply.github.com>
2022-12-08 13:58:39 -0500

    Poor Day 8 solution due to time constraints

---
+++ src/main/scala/ca/ulrichs/aoc/island/forest/Forest.scala
@@ -0,0 +1,55 @@
+package ca.ulrichs.aoc.island.forest
+
+import ca.ulrichs.aoc.core.algebra.{Coordinate, CoordinateDirection, Grid}
+import ca.ulrichs.aoc.core.input.SourceInput
+
+import scala.annotation.tailrec
+
+class Forest(val trees: Grid[Int]) {
+  lazy val visibleTrees: Seq[Coordinate] = (visibleTreeRows ++ visibleTreeColumns).distinct
+
+  lazy val visibleTreeRows: Seq[Coordinate] =
+    (for {
+      x <- 0 to trees.maximumColumn
+    } yield {
+      visibleTrees(Coordinate(x, 0), _.down) ++ visibleTrees(Coordinate(x, trees.maximumRow), _.up)
+    }).flatten.distinct
+
+  lazy val visibleTreeColumns: Seq[Coordinate] =
+    (for {
+      y <- 0 to trees.maximumRow
+    } yield {
+      visibleTrees(Coordinate(0, y), _.right) ++ visibleTrees(Coordinate(trees.maximumColumn, y), _.left)
+    }).flatten.distinct
+
+  def visibleTrees(start: Coordinate, direction: Coordinate => Coordinate): Seq[Coordinate] =
+    if trees.contains(start) then visibleTrees(direction(start), Seq(start), direction) else Seq.empty
+
+  @tailrec
+  final def visibleTrees(coordinate: Coordinate, visible: Seq[Coordinate], direction: Coordinate => Coordinate): Seq[Coordinate] = {
+    val maximumPreviousHeight = visible.flatMap(trees.at).max
+
+    trees.at(coordinate) match {
+      case Some(height) if height > maximumPreviousHeight => visibleTrees(direction(coordinate), visible :+ coordinate, direction)
+      case Some(_) => visibleTrees(direction(coordinate), visible, direction)
+      case None => visible
+    }
+  }
+
+
+  def scenicScore(at: Coordinate): Int = Coordinate.directions.Adjacent.map(d => scenicScore(at, d)).product
+
+  final def scenicScore(coordinate: Coordinate, direction: CoordinateDirection): Int =
+    trees.at(coordinate).map(height => scenicScore(height, coordinate.move(direction), 0, direction)).getOrElse(0)
+
+  final def scenicScore(referenceHeight: Int, coordinate: Coordinate, score: Int, direction: CoordinateDirection): Int =
+    trees.at(coordinate) match {
+      case Some(height) if height < referenceHeight => scenicScore(referenceHeight, coordinate.move(direction), score + 1, direction)
+      case Some(_) => score + 1
+      case None => score
+    }
+}
+
+object Forest:
+  def apply(source: SourceInput): Forest =
+    new Forest(Grid.parse(source.asSeq[String]) { (coordinate, height) => height.asDigit })
---
+++ src/test/scala/ca/ulrichs/aoc/island/forest/ForestTest.scala
@@ -0,0 +1,61 @@
+package ca.ulrichs.aoc.island.forest
+
+import ca.ulrichs.aoc.core.algebra.Coordinate
+import ca.ulrichs.aoc.core.input.SourceInput
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.funsuite.AnyFunSuite
+
+
+import scala.language.implicitConversions
+
+class ForestTest extends AnyFunSuite:
+  val exampleSource: SourceInput = SourceInput(
+    "30373",
+    "25512",
+    "65332",
+    "33549",
+    "35390"
+  )
+
+  test("Can count the number of visible tress in an example input") {
+    Forest(exampleSource).visibleTrees.length shouldBe 21
+  }
+
+  test("Can count the number of visible tress in the real forest") {
+    val source = SourceInput.fromResource("day8_tree_map")
+    Forest(source).visibleTrees.length shouldBe 1792
+  }
+
+  test("Can determine the scenic score for each point in the example forest") {
+    val forest = Forest(exampleSource)
+    forest.trees.keys.map(forest.scenicScore).max shouldBe 8
+
+    forest.scenicScore(Coordinate(2, 1)) shouldBe 4
+    forest.scenicScore(Coordinate(2, 3)) shouldBe 8
+    forest.scenicScore(Coordinate(3, 2)) shouldBe 2
+  }
+
+  test("Can determine the scenic score for each point in the real forest") {
+    val source = SourceInput.fromResource("day8_tree_map")
+    val forest = Forest(source)
+    forest.trees.keys.map(forest.scenicScore).max shouldBe 334880
+  }
+
+  test("Can count the number of visible trees in a given row") {
+    val forest = Forest(exampleSource)
+
+    forest.visibleTrees(Coordinate(0, 0), _.right) shouldBe Seq(Coordinate(0, 0), Coordinate(3, 0))
+    forest.visibleTrees(Coordinate(4, 0), _.left) shouldBe Seq(Coordinate(4, 0), Coordinate(3, 0))
+
+    forest.visibleTrees(Coordinate(0, 1), _.right) shouldBe Seq(Coordinate(0, 1), Coordinate(1, 1))
+    forest.visibleTrees(Coordinate(4, 1), _.left) shouldBe Seq(Coordinate(4, 1), Coordinate(2, 1))
+
+    forest.visibleTrees(Coordinate(0, 2), _.right) shouldBe Seq(Coordinate(0, 2))
+    forest.visibleTrees(Coordinate(4, 2), _.left) shouldBe Seq(Coordinate(4, 2), Coordinate(3, 2), Coordinate(1, 2), Coordinate(0, 2))
+
+    forest.visibleTrees(Coordinate(0, 3), _.right) shouldBe Seq(Coordinate(0, 3), Coordinate(2, 3), Coordinate(4, 3))
+    forest.visibleTrees(Coordinate(4, 3), _.left) shouldBe Seq(Coordinate(4, 3))
+
+    forest.visibleTrees(Coordinate(0, 4), _.right) shouldBe Seq(Coordinate(0, 4), Coordinate(1, 4), Coordinate(3, 4))
+    forest.visibleTrees(Coordinate(4, 4), _.left) shouldBe Seq(Coordinate(4, 4), Coordinate(3, 4))
+  }
